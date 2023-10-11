62f4ce88e3a0f963e45999b6faff3d9fdbc26aa8
Hanung Rizqi Widianto <hanung.widianto@kppmining.com>
2023-10-11 15:20:02 +0700

    init

--- restApi/Controllers/MobileController.cs
+++ restApi/Controllers/MobileController.cs
@@ -60,6 +60,7 @@ namespace restApi.Controllers
                     diastolic = a.DIASTOLIC,
                     oxygen = $"{a.OXYGEN_SATURATION}%",
                     heart_rate = $"{a.HEART_RATE} Bpm",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).FirstOrDefault();
 
                 return Ok(new { Data = data });
@@ -82,7 +83,8 @@ namespace restApi.Controllers
                     nrp = a.NRP,
                     history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
                     idchamber = a.ID_CHAMBER,
-                    temperatur_badan = $"{a.TEMPRATURE} C"
+                    temperatur_badan = $"{a.TEMPRATURE} C",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).ToList();
 
                 return Ok(new { Data = data });
@@ -105,7 +107,8 @@ namespace restApi.Controllers
                     nrp = a.NRP,
                     history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
                     idchamber = a.ID_CHAMBER,
-                    tekanan_darah = $"SYS ({a.SYSTOLIC}) / DIA ({a.DIASTOLIC})"
+                    tekanan_darah = $"SYS ({a.SYSTOLIC}) / DIA ({a.DIASTOLIC})",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).ToList();
 
                 return Ok(new { Data = data });
@@ -129,6 +132,7 @@ namespace restApi.Controllers
                     history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
                     idchamber = a.ID_CHAMBER,
                     spo02 = $"{a.OXYGEN_SATURATION}%",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).ToList();
 
                 return Ok(new { Data = data });
@@ -151,7 +155,8 @@ namespace restApi.Controllers
                     nrp = a.NRP,
                     history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
                     idchamber = a.ID_CHAMBER,
-                    heart_rate = $"{a.HEART_RATE} Bpm"
+                    heart_rate = $"{a.HEART_RATE} Bpm",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).ToList();
 
                 return Ok(new { Data = data });
@@ -177,7 +182,8 @@ namespace restApi.Controllers
                     temperatur_badan = $"{a.TEMPRATURE} C",
                     tekanan_darah = $"SYS ({a.SYSTOLIC}) / DIA ({a.DIASTOLIC})",
                     spo02 = $"{a.OXYGEN_SATURATION}%",
-                    heart_rate = $"{a.HEART_RATE} Bpm"
+                    heart_rate = $"{a.HEART_RATE} Bpm",
+                    day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
                 }).Where(a => a.nrp == nrp).ToList();
 
                 return Ok(new { Data = data });
@@ -187,5 +193,214 @@ namespace restApi.Controllers
                 return BadRequest();
             }
         }
+
+
+
+
+
+        #region Filtered
+        [HttpGet]
+        [Route("Temperatur_Badan_Filter")]
+        public IHttpActionResult Temperatur_Badan_Filter(string nrp, string startDate, string endDate)
+        {
+            try
+            {
+                db.CommandTimeout = 120;
+                IQueryable<VW_R_MOBILE> query = null;
+
+                query = db.VW_R_MOBILEs.Where(a => a.NRP == nrp);
+
+                DateTime parsedStartDate, parsedEndDate;
+                if (DateTime.TryParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedStartDate) &&
+                    DateTime.TryParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedEndDate))
+                {
+                    query = query.Where(a => a.WAKTU_ABSEN >= parsedStartDate && a.WAKTU_ABSEN <= parsedEndDate);
+                }
+
+                var dataFromDB = query.ToList();
+
+                var data = dataFromDB
+                    .Select(a => new
+                    {
+                        nrp = a.NRP,
+                        history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
+                        idchamber = a.ID_CHAMBER,
+                        temperatur_badan = $"{a.TEMPRATURE} C",
+                        day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
+                    })
+                    .OrderBy(a => a.history_tanggal)
+                    .ToList();
+
+                return Ok(new { Data = data });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("Tekanan_Darah_Filter")]
+        public IHttpActionResult Tekanan_Darah_Filter(string nrp, string startDate, string endDate)
+        {
+            try
+            {
+                db.CommandTimeout = 120;
+                IQueryable<VW_R_MOBILE> query = null;
+
+                query = db.VW_R_MOBILEs.Where(a => a.NRP == nrp);
+
+                DateTime parsedStartDate, parsedEndDate;
+                if (DateTime.TryParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedStartDate) &&
+                    DateTime.TryParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedEndDate))
+                {
+                    query = query.Where(a => a.WAKTU_ABSEN >= parsedStartDate && a.WAKTU_ABSEN <= parsedEndDate);
+                }
+
+                var dataFromDB = query.ToList();
+
+                var data = dataFromDB
+                    .Select(a => new
+                    {
+                        nrp = a.NRP,
+                        history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
+                        idchamber = a.ID_CHAMBER,
+                        tekanan_darah = $"SYS ({a.SYSTOLIC}) / DIA ({a.DIASTOLIC})",
+                        day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
+                    })
+                    .OrderBy(a => a.history_tanggal)
+                    .ToList();
+
+                return Ok(new { Data = data });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("Oxygen_Filter")]
+        public IHttpActionResult Oxygen_Filter(string nrp, string startDate, string endDate)
+        {
+            try
+            {
+                db.CommandTimeout = 120;
+                IQueryable<VW_R_MOBILE> query = null;
+
+                query = db.VW_R_MOBILEs.Where(a => a.NRP == nrp);
+
+                DateTime parsedStartDate, parsedEndDate;
+                if (DateTime.TryParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedStartDate) &&
+                    DateTime.TryParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedEndDate))
+                {
+                    query = query.Where(a => a.WAKTU_ABSEN >= parsedStartDate && a.WAKTU_ABSEN <= parsedEndDate);
+                }
+
+                var dataFromDB = query.ToList();
+
+                var data = dataFromDB
+                    .Select(a => new
+                    {
+                        nrp = a.NRP,
+                        history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
+                        idchamber = a.ID_CHAMBER,
+                        spo02 = $"{a.OXYGEN_SATURATION}%",
+                        day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
+                    })
+                    .OrderBy(a => a.history_tanggal)
+                    .ToList();
+
+                return Ok(new { Data = data });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("Heart_Rate_Filter")]
+        public IHttpActionResult Heart_Rate_Filter(string nrp, string startDate, string endDate)
+        {
+            try
+            {
+                db.CommandTimeout = 120;
+                IQueryable<VW_R_MOBILE> query = null;
+
+                query = db.VW_R_MOBILEs.Where(a => a.NRP == nrp);
+
+                DateTime parsedStartDate, parsedEndDate;
+                if (DateTime.TryParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedStartDate) &&
+                    DateTime.TryParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedEndDate))
+                {
+                    query = query.Where(a => a.WAKTU_ABSEN >= parsedStartDate && a.WAKTU_ABSEN <= parsedEndDate);
+                }
+
+                var dataFromDB = query.ToList();
+
+                var data = dataFromDB
+                    .Select(a => new
+                    {
+                        nrp = a.NRP,
+                        history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
+                        idchamber = a.ID_CHAMBER,
+                        heart_rate = $"{a.HEART_RATE} Bpm",
+                        day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
+                    })
+                    .OrderBy(a => a.history_tanggal)
+                    .ToList();
+
+                return Ok(new { Data = data });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("Data_Kesehatan_Filter")]
+        public IHttpActionResult Data_Kesehatan_Filter(string nrp, string startDate, string endDate)
+        {
+            try
+            {
+                db.CommandTimeout = 120;
+                IQueryable<VW_R_MOBILE> query = null;
+
+                query = db.VW_R_MOBILEs.Where(a => a.NRP == nrp);
+
+                DateTime parsedStartDate, parsedEndDate;
+                if (DateTime.TryParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedStartDate) &&
+                    DateTime.TryParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedEndDate))
+                {
+                    query = query.Where(a => a.WAKTU_ABSEN >= parsedStartDate && a.WAKTU_ABSEN <= parsedEndDate);
+                }
+
+                var dataFromDB = query.ToList();
+
+                var data = dataFromDB
+                    .Select(a => new
+                    {
+                        nrp = a.NRP,
+                        history_tanggal = a.WAKTU_ABSEN.Value.ToString("d MMMM yyyy | HH:mm", new CultureInfo("id-ID")),
+                        idchamber = a.ID_CHAMBER,
+                        temperatur_badan = $"{a.TEMPRATURE} C",
+                        tekanan_darah = $"SYS ({a.SYSTOLIC}) / DIA ({a.DIASTOLIC})",
+                        spo02 = $"{a.OXYGEN_SATURATION}%",
+                        heart_rate = $"{a.HEART_RATE} Bpm",
+                        day = a.WAKTU_ABSEN.Value.ToString("dddd", new CultureInfo("id-ID"))
+                    })
+                    .OrderBy(a => a.history_tanggal)
+                    .ToList();
+
+                return Ok(new { Data = data });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
     }
 }
